ccfd02e4b2aafcf38cc55f80bc66832e6f95ec4c
saketdhore <absaket.sd@gmail.com>
2025-07-30 12:07:40 -0500

    added signup route

--- backend/app/db/models/user.py
+++ backend/app/db/models/user.py
@@ -5,5 +5,6 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False) 
     hashed_password = Column(String, nullable=False)
 
